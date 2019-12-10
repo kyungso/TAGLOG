9289d909092e9c8136113af841b63d0720acc6f0
kyungso <cks678928@gmail.com>
2019-12-10 14:53:15 +0900

    add Request Body Verification

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,5 +1,6 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
+import Joi from 'joi';
 
 const { ObjectId } = mongoose.Types;
 
@@ -20,6 +21,23 @@ POST /api/posts
 }
 */
 export const write = async ctx => {
+    const schema = Joi.object().keys({
+      // 객체가 다음 필드를 가지고 있음을 검증
+      title: Joi.string().required(), // required()가 있으면 필수 항목
+      body: Joi.string().required(),
+      tags: Joi.array()
+        .items(Joi.string())
+        .required(), // 문자열로 이루어진 배열
+    });
+
+    // 검증하고 나서 검증 실패인 경우 에러 처리
+    const result = Joi.validate(ctx.request.body, schema);
+    if(result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+
     const { title, body, tags } = ctx.request.body;
     const post = new Post({
         title,
@@ -78,10 +96,29 @@ export const remove = async ctx => {
 
 /* 포스트 수정(특정 필드 변경)
 PATCH /api/posts/:id
-{ title, body }
+{ 
+  title: '수정',
+  body: '수정 내용',
+  tags: ['수정', '태그']
+}
 */
 export const update = async ctx => {
     const { id } = ctx.params;
+
+    const schema = Joi.object().keys({
+        title: Joi.string(), 
+        body: Joi.string(),
+        tags: Joi.array().items(Joi.string()), 
+    });
+  
+    // 검증하고 나서 검증 실패인 경우 에러 처리
+    const result = Joi.validate(ctx.request.body, schema);
+    if(result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+
     try {
         const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
             new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
