11a04e4462ce3b21fc6650a93884d7d1efe12074
kyungso <cks678928@gmail.com>
2019-12-10 15:02:20 +0900

    list Descending order

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -57,7 +57,9 @@ GET /api/posts
 */
 export const list = async ctx => {
     try {
-        const posts = await Post.find().exec();
+        const posts = await Post.find()
+          .sort({ _id: -1 })
+          .exec();
         ctx.body = posts;
     } catch(e) {
         ctx.throw(500, e);
