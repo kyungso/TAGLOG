538cbddfc744725b284489645bbff0a7bab398a5
kyungso <cks678928@gmail.com>
2019-12-10 17:06:32 +0900

    generate Token with SECRET_KEY

--- blog-backend/src/models/user.js
+++ blog-backend/src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt'; // 보안을 위한 단방향 해싱 함수 지원
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new Schema({
     username: String,
@@ -16,10 +17,6 @@ UserSchema.methods.checkPassword = async function(password) {
     return result; // true / false
 };
 
-UserSchema.statics.findByUsername = function(username) {
-    return this.findOne({ username });
-};
-
 // hashedPassword 필드가 응답되지 않도록 데이터를 JSON으로 변환 후, delete를 통해 삭제
 UserSchema.methods.serialize = function() {
     const data = this.toJSON();
@@ -27,5 +24,24 @@ UserSchema.methods.serialize = function() {
     return data;
 };
 
+UserSchema.methods.generateToken = function() {
+    const token = jwt.sign(
+      // 첫번째 파라미터엔 토큰 안에 집어넣고 싶은 데이터를 넣습니다
+      {
+        _id: this.id,
+        username: this.username,
+      },
+      process.env.JWT_SECRET, // 두번째 파라미터에는 JWT 암호를 넣습니다
+      {
+        expiresIn: '7d', // 7일동안 유효함
+      },
+    );
+    return token;
+  };
+
+  UserSchema.statics.findByUsername = function(username) {
+    return this.findOne({ username });
+  };
+  
 const User = mongoose.model('User', UserSchema);
 export default User;
