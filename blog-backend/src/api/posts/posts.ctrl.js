f361dfa5565e2d06cf1b5e5d9eaa7e36940c8ca5
kyungso <cks678928@gmail.com>
2019-12-10 14:34:37 +0900

    add function API

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -25,20 +25,61 @@ export const write = async ctx => {
 /* 포스트 목록 조회
 GET /api/posts
 */
-export const list = ctx => {};
+export const list = async ctx => {
+    try {
+        const posts = await Post.find().exec();
+        ctx.body = posts;
+    } catch(e) {
+        ctx.throw(500, e);
+    }
+};
 
 /* 특정 포스트 조회
 GET /api/posts/:id
 */
-export const read = ctx => {};
+export const read = async ctx => {
+    const { id } = ctx.params;
+    try {
+        const post = await Post.findById(id).exec();
+        if(!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch(e) {
+        ctx.throw(500, e);
+    }
+};
 
 /* 특정 포스트 제거
 DELETE /api/posts/:id
 */
-export const remove = ctx => {};
+export const remove = async ctx => {
+    const { id } = ctx.params;
+    try {
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
+    } catch(e) {
+        ctx.throw(500, e);
+    }
+};
 
 /* 포스트 수정(특정 필드 변경)
 PATCH /api/posts/:id
 { title, body }
 */
-export const update = ctx => {};
+export const update = async ctx => {
+    const { id } = ctx.params;
+    try {
+        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+            new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+        }).exec();
+        if(!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch(e) {
+        ctx.throw(500, e);
+    }
+};
