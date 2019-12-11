243aadb098f3f6a3b3a9201750b9be0bf128299d
kyungso <cks678928@gmail.com>
2019-12-11 17:36:10 +0900

    add function about UPDATE POSTS

--- blog-frontend/src/containers/post/PostViewerContainer.js
+++ blog-frontend/src/containers/post/PostViewerContainer.js
@@ -3,14 +3,17 @@ import { useSelector, useDispatch } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { readPost, unloadPost } from '../../modules/post';
 import PostViewer from '../../components/post/PostViewer';
+import PostActionButtons from '../../components/post/PostActionButtons';
+import { setOriginalPost } from '../../modules/write';
 
-const PostViewerContainer = ({ match }) => {
+const PostViewerContainer = ({ match, history }) => {
   const { postId } = match.params;
   const dispatch = useDispatch();
-  const { post, error, loading } = useSelector(({ post, loading }) => ({
+  const { post, error, loading, user } = useSelector(({ post, loading, user }) => ({
     post: post.post,
     error: post.error,
     loading: loading['post/READ_POST'],
+    user: user.user,
   }));
 
   useEffect(() => {
@@ -19,7 +22,21 @@ const PostViewerContainer = ({ match }) => {
       dispatch(unloadPost());
     };
   }, [dispatch, postId]);
-  return <PostViewer post={post} loading={loading} error={error} />;
+
+  const onEdit = () => {
+    dispatch(setOriginalPost(post));
+    history.push('/write');
+  };
+
+  return (
+    <PostViewer 
+      post={post} 
+      loading={loading} 
+      error={error}
+      actionButtons={<PostActionButtons onEdit={onEdit} />} 
+      ownPost={(user && user._id) === (post && post.user._id)}
+    />
+  );
 };
 
 export default withRouter(PostViewerContainer);
